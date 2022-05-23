251145dcd835f9698723faf49c79d7619b771374
justin.hobbs <justinhobbs@fastmail.com>
2022-05-23 17:40:53 -0500

    commit tree c0b1cfebcac0e80a64a5dff8356a02662ef0b4ba

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    let answer = drivers.filter(el => el === string)
+        return answer
+
+}
+
+function fuzzyMatch(drivers, string) {
+    
+}
+
+// function findMatching(drivers, name){
+//     const newDrivers = drivers.filter(function(person){return person === name});
+//     return newDrivers;
+// }
